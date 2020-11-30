c3284ebe013cd757b351ff8c99080171662abb0c
Cefalu Wave <luke.evan.graham@gmail.com>
2020-11-30 10:34:40 -0800

    better styling across the site

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,6 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-<link href="https://fonts.googleapis.com/css2?family=Asap:wght@400;500;600;700&display=swap" rel="stylesheet"></link>
       </Head>
 
       <Layout home>
